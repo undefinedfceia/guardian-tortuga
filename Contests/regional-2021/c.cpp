6f90c971718b0a6cb97963ccbe58819fddae8a17
Ignacio Kasevich <ignacio@kasevich.com.ar>
2021-07-03 15:46:27 -0300

    regional 2021

---
+++ Contests/regional-2021/c.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define pb push_back
+#define fst first
+#define snd second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> ii;
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define dprint(v) cout << #v"=" << v << endl
+#define endl "\n"
+#define tab "\t"
+
+const int MAXN=100100;
+
+int main() {
+    ios::sync_with_stdio(0);
+    
+    int n;
+    cin >> n;
+    
+    int a[n];
+    zero(a);
+
+    forn(i, n) {
+        cin >> a[i];
+    }
+
+    
+
+	return 0;
+}
